ae423f56dab489a58f8fa617f85fc183ac201c90
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-03-01 09:50:26 -0500

    Changed .jsx to .js because express does not support jsx files

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const app = require("./app"); // Import the Express app
+const app = require("./app.js"); // Import the Express app
 
 // Start the server
 app.listen(process.env.PORT, () => {
